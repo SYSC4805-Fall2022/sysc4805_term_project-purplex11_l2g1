fbd4fb13106035ff0b81d7ab5031e3d3d8af7232
Tochi-kazi <toiroakazi@gmail.com>
2022-10-30 17:26:18 -0400

    made changes

--- libraries/Motor/motor.c
+++ libraries/Motor/motor.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 
 //returns true or false if the sensor detects anything
-bool LineSensor(int pin){
+bool lineDetector(int pin){
     if(digitalRead(pin)){
         return true;
     }else{
