de5b833a77430a515c293fe6b01c4e71521fcba5
JustinWhalley-Carleton <71186152+JustinWhalley-Carleton@users.noreply.github.com>
2022-12-02 11:59:22 -0500

    Removing extra defines in main.ino

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,3 @@
-//#define FULL_INTEGRATION_TEST
-#define SKIP_FIRST_LINE_TEST
-#define OBSTACLE_DETECTION_TEST
-#define LINE_DETECTION_TEST
-#define RANDOM_WALK_TEST
-#define WHEEL_ENCODER_TEST
 #include "MainFsm.h"
 
 void setup() {
